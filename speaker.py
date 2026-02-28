513030955f2dc394d55efd0c9ecae855db224b6c
Bhuvan Teja Kudupudi <bhuvanteja.kudupudi@gmail.com>
2026-02-28 17:16:22 -0500

    fixed speaker issues

--- speaker.py
+++ speaker.py
@@ -11,7 +11,7 @@ elevenlabs = ElevenLabs(
 
 def say(t: str) -> None:
     audio = elevenlabs.text_to_speech.convert(
-        text="The first move is what sets everything in motion.",
+        text=t,
         voice_id="JBFqnCBsd6RMkjVDRZzb",
         model_id="eleven_multilingual_v2",
         output_format="mp3_44100_128",
